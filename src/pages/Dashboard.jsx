7e1a3e45667b1b520f63cdcc8362a880d20a16a6
shiinedev <shiinecadde17@gmail.com>
2025-04-16 12:01:51 +0300

    📊 feat(dashboard): add bar chart UI component from shadcd

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -4,7 +4,7 @@ import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar"
 import { AppSidebar } from '@/components/ui/app-sidebar'
 import { SiteHeader } from '@/components/ui/site-header'
 import { SectionCards } from '@/components/ui/section-cards'
-import { ChartAreaInteractive } from '@/components/ui/chart-area-interactive'
+import { BarChar } from '@/components/ui/BarChart'
 import { DataTable } from '@/components/ui/data-table'
 import data  from './data.json'
 const Dashboard = () => {
@@ -19,7 +19,7 @@ const Dashboard = () => {
             <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
               <SectionCards />
               <div className="px-4 lg:px-6">
-                <ChartAreaInteractive />
+                <BarChar />
               </div>
               <DataTable data={data} />
             </div>
