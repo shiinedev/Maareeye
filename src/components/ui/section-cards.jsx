c7297dcda32332055ef4b054261adbc174eca4a4
shiinedev <shiinecadde17@gmail.com>
2025-04-21 09:56:33 +0300

    💳 feat(ui): implement reusable dashboard  cards UI component

--- src/components/ui/section-cards.jsx
+++ src/components/ui/section-cards.jsx
@@ -9,93 +9,93 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
+import { BarChart, Wallet } from "lucide-react";
 
 export function SectionCards() {
   return (
     <div
       className="*:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card grid grid-cols-1 gap-4 px-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:shadow-xs lg:px-6 @xl/main:grid-cols-2 @5xl/main:grid-cols-4">
+       <Card className="@container/card">
+        <CardHeader>
+          <CardDescription>Balance</CardDescription>
+          <CardTitle className="text-purple-400 text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
+            45,678.00
+          </CardTitle>
+          <CardAction>
+            <div variant="outline" className={"text-purple-600"}>
+              <IconTrendingUp size={30} />
+              
+            </div>
+          </CardAction>
+        </CardHeader>
+        <CardFooter className="flex-col items-start gap-1.5 text-sm">
+          <div className="line-clamp-1 flex gap-2 font-medium">
+            Net Balance<IconTrendingUp className="size-4" />
+          </div>
+          <div className="text-muted-foreground">Net Balance</div>
+        </CardFooter>
+      </Card>
       <Card className="@container/card">
         <CardHeader>
-          <CardDescription>Total Revenue</CardDescription>
-          <CardTitle className="text-purple-400 text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
+          <CardDescription>Income</CardDescription>
+          <CardTitle className="text-green-400 text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
             $1,250.00
           </CardTitle>
           <CardAction>
-            <Badge variant="outline" className={"text-green-600"}>
-              <IconTrendingUp />
-              +12.5%
-            </Badge>
+            <div variant="outline" className={"text-green-600"}>
+            <Wallet size={30}/>
+            </div>
           </CardAction>
         </CardHeader>
         <CardFooter className="flex-col items-start gap-1.5 text-sm">
           <div className="line-clamp-1 flex gap-2 font-medium">
-            Trending up this month <IconTrendingUp className="size-4" />
+            Income  month <IconTrendingUp className="size-4" />
           </div>
           <div className="text-muted-foreground">
-            Visitors for the last 6 months
+            Income for the last  months
           </div>
         </CardFooter>
       </Card>
       <Card className="@container/card">
         <CardHeader>
-          <CardDescription>New Customers</CardDescription>
-          <CardTitle className="text-purple-400 text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
-            1,234
+          <CardDescription>Expense</CardDescription>
+          <CardTitle className="text-red-400 text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
+            $ 1,234.00
           </CardTitle>
           <CardAction>
-            <Badge variant="outline" className={"text-red-600"}>
-              <IconTrendingDown />
-              -20%
-            </Badge>
+            <div variant="outline" className={"text-red-600"}>
+              <Wallet size={30} />
+            </div>
           </CardAction>
         </CardHeader>
         <CardFooter className="flex-col items-start gap-1.5 text-sm">
           <div className="line-clamp-1 flex gap-2 font-medium">
-            Down 20% this period <IconTrendingDown className="size-4" />
+            Expense <IconTrendingDown className="size-4" />
           </div>
           <div className="text-muted-foreground">
-            Acquisition needs attention
+            Expense for the last  months
           </div>
         </CardFooter>
       </Card>
+     
       <Card className="@container/card">
         <CardHeader>
-          <CardDescription>Active Accounts</CardDescription>
-          <CardTitle className="text-purple-400 text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
-            45,678
-          </CardTitle>
-          <CardAction>
-            <Badge variant="outline" className={"text-red-600"}>
-              <IconTrendingUp />
-              -12.5%
-            </Badge>
-          </CardAction>
-        </CardHeader>
-        <CardFooter className="flex-col items-start gap-1.5 text-sm">
-          <div className="line-clamp-1 flex gap-2 font-medium">
-            Strong user retention <IconTrendingUp className="size-4" />
-          </div>
-          <div className="text-muted-foreground">Engagement exceed targets</div>
-        </CardFooter>
-      </Card>
-      <Card className="@container/card">
-        <CardHeader>
-          <CardDescription>Growth Rate</CardDescription>
-          <CardTitle className="text-purple-400 text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
+          <CardDescription>Top Category</CardDescription>
+          <CardTitle className="text-blue-400 text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
             4.5%
           </CardTitle>
           <CardAction>
-            <Badge variant="outline" className={"text-green-600"}>
-              <IconTrendingUp />
-              +4.5%
-            </Badge>
+            <div variant="outline" className={"text-blue-600"}>
+            <BarChart size={30} />
+             
+            </div>
           </CardAction>
         </CardHeader>
         <CardFooter className="flex-col items-start gap-1.5 text-sm">
           <div className="line-clamp-1 flex gap-2 font-medium">
-            Steady performance increase <IconTrendingUp className="size-4" />
+            Top Category <BarChart className="size-4" />
           </div>
-          <div className="text-muted-foreground">Meets growth projections</div>
+          <div className="text-muted-foreground">Top Category For the last Month</div>
         </CardFooter>
       </Card>
     </div>
