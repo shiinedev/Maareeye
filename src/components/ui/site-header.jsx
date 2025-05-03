b59f9e471c6187beeb1563e13d550c5d6b6006c5
shiinedev <shiinecadde17@gmail.com>
2025-05-03 18:32:25 +0300

    📌 Fix site header position & adjust bar chart padding

--- src/components/ui/site-header.jsx
+++ src/components/ui/site-header.jsx
@@ -9,8 +9,8 @@ export function SiteHeader() {
    const { theme, setTheme } = useTheme();
   return (
     <header
-      className="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
-      <div className="flex w-full items-center py-[8px]  gap-1 px-4 lg:gap-2 lg:px-6">
+      className="flex h-(--header-height) shrink-0 fixed top-0 bg-background z-20 right-0 left-0 lg:left-64 items-center gap-2 border-b group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
+      <div className="flex w-full items-center py-[12px] gap-1 px-4 lg:gap-2 lg:px-6">
         <SidebarTrigger className="-ml-1" />
         <Separator orientation="vertical" className="mx-2 data-[orientation=vertical]:h-4" />
         <Link to="/dashboard" className="text-base font-medium gradient-title">Dashboard</Link>
